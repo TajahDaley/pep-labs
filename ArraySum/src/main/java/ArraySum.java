ea75e84fb49c0cea5e3f5271b5981c143d9dc17e
Tajah Daley <tajdaley@yahoo.com>
2023-01-10 16:59:35 +0000

    Saved progress on ArraySum 2023-01-10 16:59:35 +0000

--- ArraySum/src/main/java/ArraySum.java
+++ ArraySum/src/main/java/ArraySum.java
@@ -9,7 +9,7 @@ public class ArraySum {
     public int sum(int[] arr){
         int sum = 0;
 
-        for (int i = 0; i < arr.length; i++);
+        for (int i = 0; i < arr.length; i++)
             sum = sum + arr[i];
 
         return sum;
