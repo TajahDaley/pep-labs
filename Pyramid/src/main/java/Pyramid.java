26f73a23548b46d96934bc09a172d0f7fa1e53b8
Tajah Daley <tajdaley@yahoo.com>
2023-01-27 21:07:23 +0000

    Saved progress on Pyramid 2023-01-27 21:07:23 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,6 +27,14 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        String star = " ";
+
+        for (int i = 0;i <= n;i++){
+            for (int j =0; j < 1;j++) {
+                star += "*";
+            }
+            star += "\n";
+        }
+        return star;
     }
 }
